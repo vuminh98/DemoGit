f45d22ecfb953426a7384c8e038b8b7a7a9b9f17
Minh Vu <vuminhtran027@gmail.com>
2023-04-06 15:39:01 +0700

    update3

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,5 +3,7 @@ package com.example;
 public class HelloWorld {
     public void Hello() {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld123");
+        System.out.println("HelloWorld123123");
     }
 }
